e8d8276b97a60fef5fb8fc13d4d8a12ee017d91f
Jimmy Engström <jimmy@apeoholic.se>
2022-04-27 17:19:40 +0200

    Added Attribute

---
+++ NextTechEvent/NextTechEvent.Data/DataAnnotations/MustBeFutureDateAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NextTechEvent.Data.DataAnnotations;
+
+public class MustBeFutureDateAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is DateOnly)
+        {
+            var thedate = (DateOnly?)value;
+            if (thedate == null)
+                return new ValidationResult("You must select a date", new[] { validationContext.MemberName ?? "" });
+            return thedate.Value >= DateOnly.FromDateTime(DateTime.Now.Date) ? ValidationResult.Success : new ValidationResult("The date must be today or a future date", new[] { validationContext.MemberName ?? "" });
+        }
+        else if (value is DateTime)
+        {
+            var thedate = (DateTime?)value;
+            if (thedate == null)
+                return new ValidationResult("You must select a date", new[] { validationContext.MemberName ?? "" });
+            return thedate?.Date >= DateTime.Now.Date ? ValidationResult.Success : new ValidationResult("The date must be today or a future date", new[] { validationContext.MemberName ?? "" });
+        }
+        else
+        {
+            return new ValidationResult("Must be a date type", new[] { validationContext.MemberName ?? "" });
+        }
+    }
+}
