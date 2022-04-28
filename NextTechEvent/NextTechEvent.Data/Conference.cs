de5664bafc6b9a9e028f3cf6f504dab9d35a3a84
Jimmy Engström <jimmy@apeoholic.se>
2022-04-28 09:27:27 +0200

    Worked on the AddConferencePage and added two new components for input

--- NextTechEvent/NextTechEvent.Data/Conference.cs
+++ NextTechEvent/NextTechEvent.Data/Conference.cs
@@ -1,22 +1,39 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using NextTechEvent.Data.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace NextTechEvent.Data;
 
 public class Conference
 {
     public string Id { get; set; }
-    [MinLength(3)]
+    [Required, MinLength(3)]
+    [Display(Name = "Conference name")]
     public string Name { get; set; }
+    [Url]
     public string ImageUrl { get; set; }
-    public string CfpUrl { get; set; }
-    public DateTime? CfpStartDate { get; set; }
-    public DateTime? CfpEndDate { get; set; }
+    [Url]
+    [Display(Name="Call for paper URL")]
+    public string? CfpUrl { get; set; }
+    [Display(Name = "Call for paper opens")]
+    public DateTime? CfpStartDate { get; set; } = DateTime.Now;
+    [Display(Name = "Call for paper closes")]
+    public DateTime? CfpEndDate { get; set; } = DateTime.Now;
+    [Url]
+    [Display(Name = "Event URL")]
     public string EventUrl { get; set; }
-    public DateOnly EventStart { get; set; }
-    public DateOnly EventEnd { get; set; }
+    [MustBeFutureDate]
+    [Display(Name = "When does the event start?")]
+    public DateOnly EventStart { get; set; }= DateOnly.FromDateTime(DateTime.Now);
+    [Display(Name = "When does the event end?")]
+    [MustBeFutureDate]
+    public DateOnly EventEnd { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    [Display(Name = "Where is the event?")]
     public string Venue { get; set; }
+    [Display(Name = "What country is the event in?")]
     public string Country { get; set; }
+    [Display(Name = "What city is the event in?")]
     public string City { get; set; }
+    [Display(Name = "What is the event about?")]
     public string Description { get; set; }
     public DateTime CreateDate { get; set; }
     public DateTime UpdateDate { get; set; }
@@ -27,7 +44,11 @@ public class Conference
     public double Longitude { get; set; }
     public double Latitude { get; set; }
 
+    [Display(Name = "What is the Twitter handle of the event?")]
     public string? Twitter { get; set; }
+    
+    [Url]
+    [Display(Name="URL to code of conduct")]
     public string? CodeOfConductUrl { get; set; }
 
     public bool IsOnline { get; set; }
