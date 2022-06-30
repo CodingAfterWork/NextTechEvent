7b5cf38154dc1d466dfc0395389d7a78f32d74b7
Jimmy Engström <jimmy@apeoholic.se>
2022-06-30 22:30:07 +0200

    Added Auth and cleaned up HTML

--- NextTechEvent/NextTechEvent/Program.cs
+++ NextTechEvent/NextTechEvent/Program.cs
@@ -1,8 +1,11 @@
+using Auth0.AspNetCore.Authentication;
 using Blazm.Components;
 using NextTechEvent.Data;
 using NextTechEvent.Data.Index;
 using Raven.Client.Documents;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvironmentVariables();
@@ -27,53 +30,13 @@ builder.Services.AddSingleton<IDocumentStore>(ctx =>
     return store;
 });
 
-//services.AddAuthentication(options => {
-//options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-//options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-//options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-//})
-//    .AddCookie()
-//    .AddOpenIdConnect("Auth0", options => {
-//    options.Authority = $"https://{Configuration["Auth0:Domain"]}";
+builder.Services
+    .AddAuth0WebAppAuthentication(options =>
+    {
+        options.Domain = builder.Configuration["Auth0:Authority"];
+        options.ClientId = builder.Configuration["Auth0:ClientId"];
+    });
 
-//    options.ClientId = Configuration["Auth0:ClientId"];
-//    options.ClientSecret = Configuration["Auth0:ClientSecret"];
-
-//    options.ResponseType = OpenIdConnectResponseType.Code;
-
-//    options.Scope.Clear();
-//    options.Scope.Add("openid");
-//    options.Scope.Add("profile"); // <- Optional extra
-//    options.Scope.Add("email");   // <- Optional extra
-
-//    options.CallbackPath = new PathString("/callback");
-//    options.ClaimsIssuer = "Auth0";
-//    options.SaveTokens = true;
-
-//    // Add handling of lo
-//    options.Events = new OpenIdConnectEvents
-//    {
-//        OnRedirectToIdentityProviderForSignOut = (context) =>
-//        {
-//            var logoutUri = $"https://{Configuration["Auth0:Domain"]}/v2/logout?client_id={Configuration["Auth0:ClientId"]}";
-
-//            var postLogoutUri = context.Properties.RedirectUri;
-//            if (!string.IsNullOrEmpty(postLogoutUri))
-//            {
-//                if (postLogoutUri.StartsWith("/"))
-//                {
-//                    var request = context.Request;
-//                    postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
-//                }
-//                logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
-//            }
-
-//            context.Response.Redirect(logoutUri);
-//            context.HandleResponse();
-
-//            return Task.CompletedTask;
-//        }
-//    };
 
 var app = builder.Build();
 
@@ -95,5 +58,23 @@ app.UseAuthorization();
 app.MapRazorPages(); //Do I need this?
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+app.MapGet("login", async (string redirectUri, HttpContext context) =>
+{
+    var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+         .WithRedirectUri(redirectUri)
+         .Build();
+
+    await context.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+app.MapGet("logout", async (HttpContext context) =>
+{
+    var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+         .WithRedirectUri("/")
+         .Build();
+
+    await context.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
 
 app.Run();
