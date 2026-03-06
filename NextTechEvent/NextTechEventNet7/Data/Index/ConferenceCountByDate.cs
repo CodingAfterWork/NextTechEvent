235a84aaaa2738f3326ab5201e959aad1b320951
Jimmy Engström <jimmy@apeoholic.se>
2026-03-06 20:40:53 +0100

    Deleted the .NET7 version

--- NextTechEvent/NextTechEventNet7/Data/Index/ConferenceCountByDate.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NextTechEvent.Data.Index;
-
-public class ConferenceCountByDate
-{
-    public DateOnly Date { get; set; }
-    public required string SearchTerm { get; set; }
-    public int Count { get; set; }
-}
